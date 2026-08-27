4b0355dfc51784008e951614ba8d74df63d94de4
Sameer Shaikh <developer@fehmicorp.in>
2026-08-27 15:45:07 +0530

    Changes to be committed: 	new file:   v1/cmd/go.mod 	modified:   v1/cmd/main.go 	modified:   v1/go.work 	modified:   v1/http/health.go

--- v1/http/health.go
+++ v1/http/health.go
@@ -1,23 +1,16 @@
 package http
 
-import (
-	"net/http"
-
-	"github.com/fehmicorp/pkg/v1/gateway"
-	"github.com/gin-gonic/gin"
-)
-
-func (s *gateway.Config) HealthHandler(
-	c *gin.Context,
-) {
-	responseJSON(
-		c,
-		http.StatusOK,
-		gin.H{
-			"status":      true,
-			"service":     s.Name,
-			"version":     s.Version,
-			"environment": s.Environment,
-		},
-	)
-}
+// func (s *gateway.Config) HealthHandler(
+// 	c *gin.Context,
+// ) {
+// 	responseJSON(
+// 		c,
+// 		http.StatusOK,
+// 		gin.H{
+// 			"status":      true,
+// 			"service":     s.Name,
+// 			"version":     s.Version,
+// 			"environment": s.Environment,
+// 		},
+// 	)
+// }
