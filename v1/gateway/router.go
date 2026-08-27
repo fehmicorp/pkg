5b5e2ec26a5f0ee4401bf63265c911311106378c
Sameer Shaikh <developer@fehmicorp.in>
2026-08-27 15:18:51 +0530

    Changes to be committed: 	modified:   v1/gateway/router.go

--- v1/gateway/router.go
+++ v1/gateway/router.go
@@ -5,21 +5,4 @@ import gwconfig "github.com/fehmicorp/pkg/v1/gateway/config"
 func StartServer() {
 	cfg := gwconfig.Init()
 
-	// redisClient, err := redis.NewClient(
-	// 	cfg.Redis.Host,
-	// 	int(cfg.Redis.Port),
-	// 	cfg.Redis.User,
-	// 	cfg.Redis.Password,
-	// 	cfg.Redis.DB,
-	// )
-
-	// if err != nil {
-	// 	slog.Error(
-	// 		"failed to connect redis",
-	// 		slog.String("error", err.Error()),
-	// 	)
-	// 	os.Exit(1)
-	// }
-
-	// server := httphandler.NewServer(cfg, redisClient.Client)
 }
