a799dc4c68c64c8284241c47ac8c5b182b7e7fc5
Sameer Shaikh <developer@fehmicorp.in>
2026-08-27 15:38:12 +0530

    Changes to be committed: 	new file:   cmd/main.go 	modified:   v1/config/config.go 	modified:   v1/config/go.mod 	new file:   v1/config/go.sum 	modified:   v1/gateway/config.go 	deleted:    v1/gateway/load_config.go

--- v1/config/config.go
+++ v1/config/config.go
@@ -1,11 +1,24 @@
 package config
 
-import "github.com/fehmicorp/pkg/v1/gateway"
+import (
+	"github.com/fehmicorp/pkg/v1/gateway"
+	"github.com/fehmicorp/pkg/v1/http"
+	"github.com/fehmicorp/pkg/v1/redis"
+)
 
 type Config struct {
-	App      gateway.Config `yaml:"app"`
-	Server   http.Config    `yaml:"server"`
-	Redis    RedisConfig    `yaml:"redis"`
-	Services ServicesConfig `yaml:"services"`
-	Logging  LoggingConfig  `yaml:"logging"`
+	App     gateway.Config   `yaml:"app"`
+	Server  http.Config      `yaml:"server"`
+	Redis   redis.Config     `yaml:"redis"`
+	Monitor MonitoringConfig `yaml:"monitor"`
+	Logging LoggingConfig    `yaml:"logging"`
+}
+
+type LoggingConfig struct {
+	Level string `yaml:"level" env:"LOG_LEVEL" env-default:"info"`
+	Dir   string `yaml:"dir" env:"LOG_DIR" env-default:"./logs"`
+}
+
+type MonitoringConfig struct {
+	PrometheusEnabled bool `yaml:"prometheus_enabled" env:"PROM_ENABLED" env-default:"true"`
 }
--- v1/gateway/config.go
+++ v1/gateway/config.go
@@ -5,12 +5,3 @@ type Config struct {
 	Version     string `yaml:"version" env:"APP_VERSION" env-default:"1.0.0"`
 	Environment string `yaml:"environment" env:"APP_ENV" env-default:"development"`
 }
-
-type LoggingConfig struct {
-	Level string `yaml:"level" env:"LOG_LEVEL" env-default:"info"`
-	Dir   string `yaml:"dir" env:"LOG_DIR" env-default:"./logs"`
-}
-
-type MonitoringConfig struct {
-	PrometheusEnabled bool `yaml:"prometheus_enabled" env:"PROM_ENABLED" env-default:"true"`
-}
