3896e1e4bba3496f2d0c6a01c3cd616721b5b539
Sameer Shaikh <developer@fehmicorp.in>
2026-09-07 23:28:16 +0530

    modified:   v1/utils/os/win/tray/main.go

--- v1/utils/os/win/tray/main.go
+++ v1/utils/os/win/tray/main.go
@@ -29,3 +29,25 @@ func RunMenu(
 	}
 	return systray.Tray, nil
 }
+
+func SetupTray(
+	app *application.App,
+	TrayIcon []byte,
+	Tooltip string,
+	menu []MenuOptions,
+	Quit bool,
+	onLeftClick func(),
+	onRightClick func(),
+) *application.SystemTray {
+	conf := TrayConfig{
+		IconData:     TrayIcon,
+		Tooltip:      Tooltip,
+		CustomMenus:  menu,
+		ShowQuit:     Quit,
+		OnLeftClick:  onLeftClick,
+		OnRightClick: onRightClick,
+	}
+
+	tm := NewTrayManager(app, &conf)
+	return tm.Tray
+}
